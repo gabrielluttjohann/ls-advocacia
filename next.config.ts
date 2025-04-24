65b8d5c4e823135dbfa644d05f508f9d76d7cbf5
gabrielluttjohann <gabriel.luttj@gmail.com>
2025-04-24 20:04:20 -0300

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+
+  images: {
+    dangerouslyAllowSVG: true,
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
